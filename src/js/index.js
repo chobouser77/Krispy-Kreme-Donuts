57fd54413bbaf85b6c8c18233e1f486a120685f6
twinogre <glad2you@gmail.com>
2024-12-10 18:35:01 +0900

    섹션 5까지 완성 , 임시저장

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 $(document).ready(function (){
   var swiper1 = new Swiper(".s5swiper", {
-    slidesPerView: 3,
+    slidesPerView: "auto",
+    centeredSlides: true,
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    loop: true,
   });
 });
